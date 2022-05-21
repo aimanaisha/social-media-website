5b2bcdc192327c84c62645dc6f3bcb9d097de181
Aiman <aimanaisha851@gmail.com>
2022-05-21 14:17:57 +0530

    minor changes

--- social-media-app/src/components/CreatePost.js
+++ social-media-app/src/components/CreatePost.js
@@ -5,101 +5,211 @@ import { uploadBytes, ref, getDownloadURL } from "firebase/storage";
 import { storage, db } from "../store/firebase";
 import { addDoc, collection,onSnapshot } from "firebase/firestore";
 
+// const CreatePost = () => {
+
+    // const currentUser = useAuth()
+    // const textInputRef = useRef()
+    // const [loading, setLoading] = useState(false)
+    // const [file, setFile] = useState(null)
+    // const [photoURL, setPhotoURL] = useState([])
+    // const [status, setStatus] = useState([])
+
+    // const selectFileHandler = (event) => {
+    //     if(event.target.files[0]){
+    //         setFile(event.target.files[0])
+    //     }
+    //     else{
+    //         alert('Nothing is selected')
+    //     }
+    // }
+
+//     const uploadFileHandler = async () => {
+//         if(file){
+        //     setLoading(true)
+        // console.log(file)
+        // const fileRef = ref(storage, `posts/${currentUser.uid}/${file.name}.png` )
+        // await uploadBytes(fileRef, file)
+        // const photoURL = await getDownloadURL(fileRef);
+
+        // console.log(photoURL)
+
+        // const collectionRef = collection(db, 'file_posts')
+        // const payload = {file_name: `${file.name}.png`, file_url: photoURL, posted_by: currentUser.uid}
+        // await addDoc(collectionRef, payload)       
+        // setLoading(false)
+//         }
+//         else{
+//             alert('choose a file')
+//         }        
+//     }
+
+//     useEffect(() => {
+//         setLoading(true)
+//         const unsub = onSnapshot(collection(db, 'file_posts'), (snapshot)=>{
+//             setPhotoURL(snapshot.docs.map(doc => doc.data().file_url))
+//             console.log(photoURL)
+//             setLoading(false)
+//         })
+//         return unsub
+//     },[])
+
+//     const uploadTextHandler = async () => {
+//         const text = textInputRef.current.value.trim()
+//         if(text.length !== 0){
+//             setLoading(true)
+//             const collectionRef = collection(db, 'text_posts')
+//             const payload = {posted_by: currentUser.uid, text_content: text}
+//             await addDoc(collectionRef, payload)
+//             setLoading(false)
+
+//         }
+//         else{
+//             alert("Empty Field!")
+//         }
+//     }
+
+//     useEffect(() => {
+//         setLoading(true)
+//         const unsub = onSnapshot(collection(db, 'text_posts'), (snapshot)=>{
+//             setStatus(snapshot.docs.map(doc => doc.data().text_content))
+//             console.log(status)
+//             setLoading(false)
+//         })
+//         return unsub
+//     },[])
+
+//     return(
+//         <div>
+//             <label htmlFor='file-upload'>Upload file</label>
+//             <input id='file-upload' type='file' onChange={selectFileHandler}/>
+//             <button onClick={uploadFileHandler}>Post</button>
+
+//             <label htmlFor='text-upload'>What's on Your Mind?</label>
+//             <input id='text-upload' type='text' ref={textInputRef}/>
+//             <button onClick={uploadTextHandler}>Post</button>
+
+//             <p>{loading && 'loading...'}</p>
+            
+//             {photoURL.map((url)=>{
+//                 return <img src={url} alt=''/>
+//             })}
+
+//             {status.map((text)=>{
+//                 return <p>{text}</p>
+//             })}
+            
+//         </div>
+//     )
+// }
+
+// export default CreatePost
+
 const CreatePost = () => {
+
 
     const currentUser = useAuth()
     const textInputRef = useRef()
     const [loading, setLoading] = useState(false)
     const [file, setFile] = useState(null)
-    const [photoURL, setPhotoURL] = useState([])
-    const [status, setStatus] = useState([])
+    // const [photoURL, setPhotoURL] = useState()
+    const [remove, setRemove] = useState(false)
+    // const [status, setStatus] = useState([])
+    const [post, setPost] = useState([])
+
+
+    const [URL, setURL] = useState([])
+    const [show, setShow] = useState([])
+    
+
 
     const selectFileHandler = (event) => {
         if(event.target.files[0]){
             setFile(event.target.files[0])
-        }
-        else{
-            alert('Nothing is selected')
+            setRemove(true)
         }
     }
 
-    const uploadFileHandler = async () => {
-        if(file){
-            setLoading(true)
-        console.log(file)
-        const fileRef = ref(storage, `posts/${currentUser.uid}/${file.name}.png` )
-        await uploadBytes(fileRef, file)
-        const photoURL = await getDownloadURL(fileRef);
 
-        console.log(photoURL)
+    const formSubmitHandler = async (e) => {
+        e.preventDefault()
+        setRemove(false)
+        setFile(null)
 
-        const collectionRef = collection(db, 'file_posts')
-        const payload = {file_name: `${file.name}.png`, file_url: photoURL, posted_by: currentUser.uid}
-        await addDoc(collectionRef, payload)       
-        setLoading(false)
-        }
-        else{
-            alert('choose a file')
-        }        
-    }
-
-    useEffect(() => {
-        setLoading(true)
-        const unsub = onSnapshot(collection(db, 'file_posts'), (snapshot)=>{
-            setPhotoURL(snapshot.docs.map(doc => doc.data().file_url))
-            console.log(photoURL)
-            setLoading(false)
-        })
-        return unsub
-    },[])
-
-    const uploadTextHandler = async () => {
         const text = textInputRef.current.value.trim()
-        if(text.length !== 0){
-            setLoading(true)
-            const collectionRef = collection(db, 'text_posts')
-            const payload = {posted_by: currentUser.uid, text_content: text}
-            await addDoc(collectionRef, payload)
-            setLoading(false)
 
+        let textPost 
+        if(text === ''){
+            textPost = null
+        }else{
+            textPost = text
+        }
+
+        console.log(file)
+        console.log(text)
+
+        setLoading(true)
+
+        if(file){
+            const fileRef = ref(storage, `posts/${currentUser.uid}/${file.name}.png` )
+            await uploadBytes(fileRef, file)
+            const photoURL = await getDownloadURL(fileRef);
+
+            const collectionRef = collection(db, 'posts')
+            const payload = { file_url: photoURL, text: textPost, posted_by: currentUser.uid }
+            await addDoc(collectionRef, payload)
+        
         }
         else{
-            alert("Empty Field!")
+            const collectionRef = collection(db, 'posts')
+            const payload = { file_url: null, text: textPost, posted_by: currentUser.uid }
+            await addDoc(collectionRef, payload)
         }
+            
+        setFile(null)        
     }
+
+    const allData = []
 
     useEffect(() => {
         setLoading(true)
-        const unsub = onSnapshot(collection(db, 'text_posts'), (snapshot)=>{
-            setStatus(snapshot.docs.map(doc => doc.data().text_content))
-            console.log(status)
+        const unsub = onSnapshot(collection(db, 'posts'), (snapshot)=>{
+            // console.log(snapshot.docs[0].data()['text'])
+            // console.log(snapshot.docs[0].data()['file_url'])
+            // const allData = [{
+            //     "text": snapshot.docs[0].data()['text']
+            // }]
+
+         snapshot.docs.map((doc) =>{
+                const tempData = {
+                    "text": doc.data()['text'],
+                    "file": doc.data()['file_url']
+                }
+
+          allData.push(tempData)
+            });                    
+
+            
             setLoading(false)
         })
         return unsub
-    },[])
+        },[file])
+    console.log(allData)
+
+    const removeFileHandler = () => {
+        setFile(null)
+        setRemove(false)
+    }
 
     return(
-        <div>
-            <label htmlFor='file-upload'>Upload file</label>
-            <input id='file-upload' type='file' onChange={selectFileHandler}/>
-            <button onClick={uploadFileHandler}>Post</button>
-
-            <label htmlFor='text-upload'>What's on Your Mind?</label>
-            <input id='text-upload' type='text' ref={textInputRef}/>
-            <button onClick={uploadTextHandler}>Post</button>
-
-            <p>{loading && 'loading...'}</p>
+        <form onSubmit={formSubmitHandler}>
+            <input type='file' onChange={selectFileHandler}/>
             
-            {photoURL.map((url)=>{
-                return <img src={url} alt=''/>
-            })}
+            { remove && <button onClick={removeFileHandler} type='button'>remove</button>}
+             
 
-            {status.map((text)=>{
-                return <p>{text}</p>
-            })}
-            
-        </div>
-    )
+            <textarea rows="4" cols="50" ref={textInputRef}></textarea>
+            <button>Post</button>
+
+        </form>)
 }
-
 export default CreatePost
